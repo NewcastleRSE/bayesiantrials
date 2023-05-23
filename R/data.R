784f2aee279a8a75502c2473aafa89b7e1d449ad
kathryn-garside <Kathryn.garside@newcastle.ac.uk>
2023-05-23 13:21:55 +0100

    added icons icc data and initial docs and vignette

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Identifying Continence OptioNs after Stroke (ICONS)
+#'
+#' Estimates of the intra-cluster correlation coefficient based on a
+#' feasibility study Thomas (2015).
+#'
+#' @format ## `icons_icc_data`
+#' A data frame with 10,001 rows and 2 columns:
+#' \describe{
+#'   \item{V1}{Index}
+#'   \item{V2}{ICC estimate}
+#'}
+#'
+#' @references
+#' Thomas, Lois H, Beverley French, Christopher J Sutton,
+#' Denise Forshaw, Michael J Leathley, Christopher R Burton,
+#' Brenda Roe, et al. 2015.
+#' “Identifying Continence OptioNs After Stroke (ICONS):
+#' An Evidence Synthesis, Case Study and Exploratory Cluster
+#' Randomised Controlled Trial of the Introduction of a Systematic
+#' Voiding Programme for Patients with Urinary Incontinence After
+#' Stroke in Secondary Care.” Programme Grants for Applied Research 3 (1).
+"icons_icc_data"
